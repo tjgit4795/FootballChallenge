a0c77d3e810932f6017f55fe7ebae3e17f998e3a
Tim Ruhl <timjruhl@gmail.com>
2025-02-17 18:46:29 -0500

    Initial Commit

---
+++ FootballChallenge/FootballChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballChallengeApp.swift
+//  FootballChallenge
+//
+//  Created by Michelle Ruhl on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
